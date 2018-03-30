2e495cf5eca8cee59fd41824a372d7cefadcf97e
Neuy Chaosreng <sovon.khom168@gmail.com>
2018-03-30 10:12:01 +0700

    customer resoures routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   resources :skills
-  resources :portfolios
-  resources :blogs
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+  resources :blogs, only: %i[new show index]
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

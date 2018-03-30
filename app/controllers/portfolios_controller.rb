f9983fad8e733c1a49b4510af0b57dee0ac3b4c8
Neuy Chaosreng <sovon.khom168@gmail.com>
2018-03-30 09:28:00 +0700

    implement portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -68,6 +68,6 @@ class PortfoliosController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def portfolio_params
-    params.require(:portfolio).permit(:title, :subtitle)
+    params.require(:portfolio).permit(:title, :subtitle, :body)
   end
 end

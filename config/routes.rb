8ad03a95ab4d81563a0e61b3130c96af0675a2b0
Neuy Chaosreng <sovon.khom168@gmail.com>
2018-03-30 09:44:45 +0700

    implement custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  root to: 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
   resources :skills
   resources :portfolios
   resources :blogs
